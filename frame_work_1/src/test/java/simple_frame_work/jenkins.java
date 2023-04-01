028b2f9a1771b02581879e74309c6c0337fa5e12
golfershot <callmehemanth573@gmail.com>
2023-04-01 13:07:41 +0530

    "new file and updated file"

--- frame_work_1/src/test/java/simple_frame_work/jenkins.java
+++ frame_work_1/src/test/java/simple_frame_work/jenkins.java
@@ -6,6 +6,7 @@ public class jenkins {
 		// TODO Auto-generated method stub
 		System.out.println("hello");
 		System.out.println("this is from cmd");
+		System.out.println("testing");
 	}
 
 }
---
+++ frame_work_1/src/test/java/simple_frame_work/tesing_for_jenkins.java
@@ -0,0 +1,10 @@
+package simple_frame_work;
+
+public class tesing_for_jenkins {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello jenkins");
+	}
+
+}
